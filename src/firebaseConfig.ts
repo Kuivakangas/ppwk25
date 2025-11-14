c2bbd14d852ecbed68dc500ed25d4fc5b71d3fdc
Kuivakangas <bgy052@haaga-helia.fi>
2025-11-14 09:06:37 +0200

    commit

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -16,6 +16,7 @@ const firebaseConfig = {
   measurementId: "G-VZ5DKMQTKX"
 };
 
+export default firebaseConfig;
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
